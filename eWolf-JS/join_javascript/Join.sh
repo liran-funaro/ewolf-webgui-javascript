4857569de2575fe86d332c9ae206e0dcba32fd81
Liran Funaro <fonaro@gmail.com>
2012-06-12 20:17:25 +0300

    updated javascript join script

--- eWolf-JS/join_javascript/Join.sh
+++ eWolf-JS/join_javascript/Join.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat $@ > ./output/core.js
-java -jar ./Script/yuicompressor-2.4.7.jar --type js ./output/core.js -o ../../ewolf-server/E-Wolf-Server/src/il/technion/ewolf/server/core.mini.js
-
+cat *.js > core.js.temp
+java -jar ./join_javascript/yuicompressor-2.4.7.jar --type js core.js.temp -o ../eWolf-Java/server_resources/core.mini.js
+rm core.js.temp
