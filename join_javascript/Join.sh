4e0d6a46b4c08c0b00e947a966a1067abc33d31b
Liran Funaro <fonaro@gmail.com>
2012-07-07 05:37:41 +0300

    - Compress CSS as well

--- join_javascript/Join.sh
+++ join_javascript/Join.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 cat *.js > core.js.temp
-cat CSS/*.css > ../ewolf-webgui/server_resources/www/home.css
+cat CSS/*.css > CSS/home.css.temp
 
 java -jar ./join_javascript/yuicompressor-2.4.7.jar --type js core.js.temp -o core.mini.js.temp
 rm core.js.temp
 
+java -jar ./join_javascript/yuicompressor-2.4.7.jar --type css CSS/home.css.temp -o CSS/home.mini.css.temp
+rm CSS/home.css.temp
+
 rm ../ewolf-webgui/server_resources/www/core.mini.js
 mv core.mini.js.temp ../ewolf-webgui/server_resources/www/core.mini.js
 
+rm ../ewolf-webgui/server_resources/www/home.css
+mv CSS/home.mini.css.temp ../ewolf-webgui/server_resources/www/home.css
